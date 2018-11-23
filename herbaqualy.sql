dbbfb683c907e94a672ca870d38cd832934cc9a3
crizue <educrizu@gmail.com>
2018-11-22 22:36:35 -0200

    as

--- herbaqualy.sql
+++ herbaqualy.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 29/08/2018 às 14:27
--- Versão do servidor: 5.7.21-0ubuntu0.16.04.1
--- Versão do PHP: 7.0.22-0ubuntu0.16.04.1
+-- Host: 127.0.0.1
+-- Generation Time: 23-Nov-2018 às 01:35
+-- Versão do servidor: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,115 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `herbaqualy`
+-- Database: `herbaqualy`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categoria`
---
-
-CREATE TABLE `categoria` (
-  `id_categoria` int(11) NOT NULL,
-  `descricao_cate` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Fazendo dump de dados para tabela `categoria`
---
-
-INSERT INTO `categoria` (`id_categoria`, `descricao_cate`) VALUES
-(1, 'cha'),
-(2, 'fruta\r\n');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `comentario`
---
-
-CREATE TABLE `comentario` (
-  `id_comentario` int(11) NOT NULL,
-  `data_hota` datetime DEFAULT NULL,
-  `texto_comen` varchar(7000) DEFAULT NULL,
-  `id_pergunta` int(11) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pergunta`
---
-
-CREATE TABLE `pergunta` (
-  `detalhamento_per` varchar(7000) DEFAULT NULL,
-  `pergunta` varchar(100) DEFAULT NULL,
-  `id_pergunta` int(11) NOT NULL,
-  `data_hora` datetime DEFAULT CURRENT_TIMESTAMP,
-  `id_user` int(11) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Fazendo dump de dados para tabela `pergunta`
---
-
-INSERT INTO `pergunta` (`detalhamento_per`, `pergunta`, `id_pergunta`, `data_hora`, `id_user`, `id_categoria`) VALUES
-('1', '1', 1, '2018-08-15 00:00:00', 1, 2);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `planta`
---
-
-CREATE TABLE `planta` (
-  `id_planta` int(11) NOT NULL,
-  `titulo` varchar(100) DEFAULT NULL,
-  `descricao_plan` varchar(7000) DEFAULT NULL,
-  `id_categoria` int(11) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `resposta`
---
-
-CREATE TABLE `resposta` (
-  `id_resposta` int(11) NOT NULL,
-  `data_hora` datetime DEFAULT NULL,
-  `texto_res` varchar(7000) DEFAULT NULL,
-  `id_pergunta` int(11) DEFAULT NULL,
-  `id_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `tip_user`
---
-
-CREATE TABLE `tip_user` (
-  `id_tip_user` int(11) NOT NULL,
-  `descricao_tip` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Fazendo dump de dados para tabela `tip_user`
---
-
-INSERT INTO `tip_user` (`id_tip_user`, `descricao_tip`) VALUES
-(1, 'admin'),
-(2, 'comum\r\n');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `usuario`
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
@@ -138,143 +38,45 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id_user`, `nome`, `login`, `senha`, `email`, `id_tip_user`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin@admin', 1);
+(1, 'admin', 'admin', 'admin', 'admin@admin', 1),
+(3, 'aa', 'aa', 'aa', 'aa@aa', 2),
+(4, 'bb', 'bb', 'bb', 'bb@bb', 3);
 
 --
--- Índices de tabelas apagadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`id_categoria`);
-
---
--- Índices de tabela `comentario`
---
-ALTER TABLE `comentario`
-  ADD PRIMARY KEY (`id_comentario`),
-  ADD KEY `id_pergunta` (`id_pergunta`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Índices de tabela `pergunta`
---
-ALTER TABLE `pergunta`
-  ADD PRIMARY KEY (`id_pergunta`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_categoria` (`id_categoria`);
-
---
--- Índices de tabela `planta`
---
-ALTER TABLE `planta`
-  ADD PRIMARY KEY (`id_planta`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_categoria` (`id_categoria`);
-
---
--- Índices de tabela `resposta`
---
-ALTER TABLE `resposta`
-  ADD PRIMARY KEY (`id_resposta`),
-  ADD KEY `id_pergunta` (`id_pergunta`),
-  ADD KEY `id_user` (`id_user`);
-
---
--- Índices de tabela `tip_user`
---
-ALTER TABLE `tip_user`
-  ADD PRIMARY KEY (`id_tip_user`);
-
---
--- Índices de tabela `usuario`
+-- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_user`),
   ADD KEY `id_tip_user` (`id_tip_user`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `categoria`
---
-ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de tabela `comentario`
---
-ALTER TABLE `comentario`
-  MODIFY `id_comentario` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de tabela `pergunta`
---
-ALTER TABLE `pergunta`
-  MODIFY `id_pergunta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT de tabela `planta`
---
-ALTER TABLE `planta`
-  MODIFY `id_planta` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de tabela `resposta`
---
-ALTER TABLE `resposta`
-  MODIFY `id_resposta` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de tabela `tip_user`
---
-ALTER TABLE `tip_user`
-  MODIFY `id_tip_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de tabela `usuario`
+-- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
--- Restrições para dumps de tabelas
+-- Constraints for dumped tables
 --
 
 --
--- Restrições para tabelas `comentario`
---
-ALTER TABLE `comentario`
-  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`id_pergunta`) REFERENCES `pergunta` (`id_pergunta`),
-  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`);
-
---
--- Restrições para tabelas `pergunta`
---
-ALTER TABLE `pergunta`
-  ADD CONSTRAINT `pergunta_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`),
-  ADD CONSTRAINT `pergunta_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
-
---
--- Restrições para tabelas `planta`
---
-ALTER TABLE `planta`
-  ADD CONSTRAINT `planta_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`),
-  ADD CONSTRAINT `planta_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id_categoria`);
-
---
--- Restrições para tabelas `resposta`
---
-ALTER TABLE `resposta`
-  ADD CONSTRAINT `resposta_ibfk_1` FOREIGN KEY (`id_pergunta`) REFERENCES `pergunta` (`id_pergunta`),
-  ADD CONSTRAINT `resposta_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_user`);
-
---
--- Restrições para tabelas `usuario`
+-- Limitadores para a tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_tip_user`) REFERENCES `tip_user` (`id_tip_user`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
